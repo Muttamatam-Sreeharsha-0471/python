e50b250a8c9223ab5a27f6efe3b33937fc3a4a2e
Muttamatam-Sreeharsha-0471 <112851835+Muttamatam-Sreeharsha-0471@users.noreply.github.com>
2022-09-05 13:01:43 +0530

    Add files via upload

---
+++ principle amount to simple interest.py
@@ -0,0 +1,5 @@
+Principle_amount=int(input("enter the principle amount:"))
+Rate_of_intrest=int(input("enter the rate of interest:"))
+Time_period=int(input("enter number of years:"))
+SimpleIntrest=Principle_amount*Time_period*Rate_of_intrest/100
+print(SimpleIntrest)
