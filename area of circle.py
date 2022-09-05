b870bb8215607f72ec71aca0126db5a11c0a4e7f
Muttamatam-Sreeharsha-0471 <112851835+Muttamatam-Sreeharsha-0471@users.noreply.github.com>
2022-09-05 13:02:51 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,3 @@
+r=float(input("enter the radious of the circle:"));
+area_of_the_circle= 22/7*r*r;
+print("area of the circle:",area_of_the_circle)
---
+++ celcius to farenheit.py
@@ -0,0 +1,3 @@
+celsius=int(input("enter the temperature in celsius="));
+fahrenheit=celsius+273;
+print("fahrenheit of your entered celsius value is:",fahrenheit)
