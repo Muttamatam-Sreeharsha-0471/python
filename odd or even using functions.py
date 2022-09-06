09d773028ad4d4b708bdb41842ca41eea9cca9fc
Muttamatam-Sreeharsha-0471 <112851835+Muttamatam-Sreeharsha-0471@users.noreply.github.com>
2022-09-06 11:14:08 +0530

    Add files via upload

---
+++ odd or even using functions.py
@@ -0,0 +1,7 @@
+n=int(input("enter the number to check whether the number is even or odd:"))
+def find_evenodd(n):
+    if(n%2==0):
+        print("the number is EVEN.")
+    else:
+        print("The number is ODD.")
+find_evenodd(n);
