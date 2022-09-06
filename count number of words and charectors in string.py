b40635eba5ffea2e3fa0958e92b99faf118effe5
Muttamatam-Sreeharsha-0471 <112851835+Muttamatam-Sreeharsha-0471@users.noreply.github.com>
2022-09-06 12:33:06 +0530

    Add files via upload

---
+++ count number of words and charectors in string.py
@@ -0,0 +1,5 @@
+string=str(input("enter any string:"))
+print("the original string is :",string)
+res=len(string.split())
+print("the number of words in that sentence is:",str(res))
+print("the number of charectors in that sentence is:",len(string))
