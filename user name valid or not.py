c53b1527f4ccea1587cdeef9f314d6d4cee903a7
Muttamatam-Sreeharsha-0471 <112851835+Muttamatam-Sreeharsha-0471@users.noreply.github.com>
2022-09-07 10:53:45 +0530

    Add files via upload

---
+++ user name valid or not.py
@@ -0,0 +1,7 @@
+user_name=input("enter the user name:")
+reenter_user_name=input("reenter the user name:")
+
+if(user_name == reenter_user_name):
+    print("it is a vaild user name")
+else:
+    print("it is not a valid user name")
