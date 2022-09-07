da1cf6e2ce25c37c47bc1928395e2a9e8b1c3df1
Muttamatam-Sreeharsha-0471 <112851835+Muttamatam-Sreeharsha-0471@users.noreply.github.com>
2022-09-07 10:57:24 +0530

    Update calculator in python.py
    added the power operation in this program

--- calculator in python.py
+++ calculator in python.py
@@ -6,17 +6,20 @@ def mul(a,b):
     return a*b
 def div(a,b):
     return a/b
+def pow(a,b):
+    return a**b
 
 print("select operator:")
 print("1.+")
 print("2.-")
 print("3.x")
 print("4./")
+print("5.^")
 
 while True:
-    choice = input("enter choice(1/2/3/4):")
+    choice = input("enter choice(1/2/3/4/5):")
 
-    if (choice in ('1','2','3','4')):
+    if (choice in ('1','2','3','4','5')):
         a=float(input("enter first number:"))
         b=float(input("enter second number:"))
 
@@ -28,6 +31,8 @@ while True:
            print(a,"x",b,"=",mul(a,b))
         elif(choice=="4"):
            print(a,"/",b,"=",div(a,b))
+        elif(choice=="5"):
+            print(a,"^",b,"=",pow(a,b))
 
         next_calculation=input("lets do next calculation?(yes/no):")
         if(next_calculation=="no"):
