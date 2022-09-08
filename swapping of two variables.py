7d5489f7f433e8767456b2d8b82830938bf2e2e1
Muttamatam-Sreeharsha-0471 <112851835+Muttamatam-Sreeharsha-0471@users.noreply.github.com>
2022-09-08 10:05:45 +0530

    Add files via upload

---
+++ swapping of two variables.py
@@ -0,0 +1,8 @@
+a=int(input("enter any number A:"))
+b=int(input("enter any number B:"))
+temp=a
+a=b
+b=temp
+
+print("the value of A after swapping is:",format(a))
+print("the value of B after swapping is:",format(b))
