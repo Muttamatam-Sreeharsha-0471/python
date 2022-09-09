96ac30c1c3654b3c17e74a34e91804e7e2602a0d
Muttamatam-Sreeharsha-0471 <112851835+Muttamatam-Sreeharsha-0471@users.noreply.github.com>
2022-09-09 09:59:44 +0530

    Update square star pattren.py

--- square star pattren.py
+++ square star pattren.py
@@ -1,5 +1,5 @@
 side=int(input("Enter the value of square:"))
-print("rectangle star pattern:-")
+print("Square star pattern:-")
 for i in range(side):
     for j in range(side):
         print("*",end=" ")
