40a024051d54befe97033f2c8107060ca3ce5fb0
Muttamatam Sreeharsha <112851835+Muttamatam-Sreeharsha-0471@users.noreply.github.com>
2022-11-16 21:44:30 +0530

    Update perfect number or not .py

--- perfect number or not .py
+++ perfect number or not .py
@@ -4,7 +4,7 @@ for i in range(1,n):
     if(n%i==0):
         sum1=sum1+i
 if(sum1==n):
-    print(n,"is perfect number!")
+    print(n,"is perfect number.")
 else:
     print(n,"is not perfect number.")
 
